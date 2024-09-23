c8d178cf9ded1aad6abcf4d8737848096eec6e96
Rakib010 <hossainrokib55@gmail.com>
2024-09-23 13:57:17 +0600

    using function same card

---
+++ js/calculate.js
@@ -0,0 +1,23 @@
+
+/* function calculateDonation() {
+  const input = parseFloat(document.getElementById("input").value);
+  const addTaka = parseFloat(document.getElementById("add-taka").innerText);
+
+  if (input <= 0 || isNaN(input)) {
+    alert("Invalid Donation amount");
+    return;
+  }
+  const newBalance = input + addTaka;
+
+  document.getElementById("add-taka").innerText = newBalance;
+
+  const lessAmount = parseFloat(
+    document.getElementById("less-amount").innerText
+  );
+
+  const mainBalance = lessAmount - input;
+
+  return (document.getElementById("less-amount").innerText = mainBalance);
+
+  
+} */
