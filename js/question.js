b218677262256681aa5968a3e4757cdfe5e52d46
Rakib010 <hossainrokib55@gmail.com>
2024-09-24 21:10:43 +0600

    optimaization code using function

--- js/question.js
+++ js/question.js
@@ -1,5 +1,5 @@
 // home to blog btn
 
 document.getElementById("home-btn").addEventListener("click", function () {
-  window.location.href = "/index.html";
+  window.location.href = "index.html";
 });
--- js/script.js
+++ js/script.js
@@ -1,117 +1,19 @@
-//
-// Blog to Home btn
+// Blog to Home button
 document.getElementById("blog-btn").addEventListener("click", function () {
-  // document.getElementById("blog-btn").innerText = "Home";
   window.location.href = "question.html";
 });
 
-// donate btn 1
+// event listeners for the buttons
 document.getElementById("donate-btn1").addEventListener("click", function () {
-  // calculateDonation("input", "add-taka");
-  const input = parseFloat(document.getElementById("input").value);
-  const addTaka = parseFloat(document.getElementById("add-taka").innerText);
-
-  if (input <= 0 || isNaN(input)) {
-    alert("Invalid Donation amount");
-    return;
-  }
-
-  const newBalance = input + addTaka;
-
-  document.getElementById("add-taka").innerText = newBalance;
-
-  const lessAmount = parseFloat(
-    document.getElementById("less-amount").innerText
-  );
-
-  const mainBalance = lessAmount - input;
-
-  document.getElementById("less-amount").innerText = mainBalance;
-
-  //history added
-  const heading = document.getElementById("h2-tag").innerText;
-  const historyItem = document.createElement("div");
-  historyItem.className = "p-4 rounded-lg shadow-md border-2 mt-5 mb-5";
-
-  historyItem.innerHTML = `
-    <p class="font-bold">${input} Taka is ${heading}</p>
-    <p>Date:${new Date().toLocaleString()}</p>
-  
-  `;
-  document.getElementById("history-items").appendChild(historyItem);
-
-  //popup
-  document.getElementById("popup").classList.remove("hidden");
+  handleDonation("input", "add-taka", "h2-tag");
 });
-// donate btn 2
+
 document.getElementById("donate-btn2").addEventListener("click", function () {
-  // calculateDonation
-
-  // calculateDonation("input2","add-taka2");
-
-  const input = parseFloat(document.getElementById("input2").value);
-  const addTaka = parseFloat(document.getElementById("add-taka2").innerText);
-
-  if (input <= 0 || isNaN(input)) {
-    alert("Invalid Donation amount");
-    return;
-  }
-  const newBalance = input + addTaka;
-
-  document.getElementById("add-taka2").innerText = newBalance;
-
-  const lessAmount = parseFloat(
-    document.getElementById("less-amount").innerText
-  );
-
-  const mainBalance = lessAmount - input;
-
-  document.getElementById("less-amount").innerText = mainBalance;
-
-  //history added
-  const heading = document.getElementById("h2-tag2").innerText;
-  const historyItem = document.createElement("div");
-  historyItem.className = "p-4 rounded-lg shadow-md border-2 mt-5 mb-5";
-
-  historyItem.innerHTML = `
-     <p class="font-bold">${input} Taka is ${heading}</p>
-     <p>Date:${new Date().toLocaleString()}</p>
-   
-   `;
-  document.getElementById("history-items").appendChild(historyItem);
+  handleDonation("input2", "add-taka2", "h2-tag2");
 });
-// donate btn 3
+
 document.getElementById("donate-btn3").addEventListener("click", function () {
-  const input = parseFloat(document.getElementById("input3").value);
-  const addTaka = parseFloat(document.getElementById("add-taka3").innerText);
-
-  if (input <= 0 || isNaN(input)) {
-    alert("Invalid Donation amount");
-    return;
-  }
-  const newBalance = input + addTaka;
-
-  document.getElementById("add-taka3").innerText = newBalance;
-
-  const lessAmount = parseFloat(
-    document.getElementById("less-amount").innerText
-  );
-
-  const mainBalance = lessAmount - input;
-
-  document.getElementById("less-amount").innerText = mainBalance;
-
-  //history added
-  const heading = document.getElementById("h2-tag3").innerText;
-  const historyItem = document.createElement("div");
-  historyItem.className = "p-4 rounded-lg shadow-md border-2 mt-5 mb-5";
-
-  historyItem.innerHTML = `
-    <p class="font-bold">${input} Taka is Donated for ${heading}</p>
-    <p>Date:${new Date().toLocaleString()}</p>
-  
-  `;
-  document.getElementById("history-items").appendChild(historyItem);
+  handleDonation("input3", "add-taka3", "h2-tag3");
 });
 
 // donation & History
